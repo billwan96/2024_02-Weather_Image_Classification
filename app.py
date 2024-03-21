14ace94b35779424bb1f551afd2b588b7ce758ca
billwan96 <billwan@student.ubc.ca>
2024-03-21 14:14:55 -0700

    update app

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ data_transforms = transforms.Compose([
     transforms.ToTensor(),
 ])
 
+@st.cache_resource
 def load_model():
     # Define the model architecture
     densenet = models.densenet121(pretrained=False)
